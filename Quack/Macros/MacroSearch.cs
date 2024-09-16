51cda0ab9b3e8ba21600b43a0ad6e97941963c4d
anya-hichu <anya_hichu@outlook.com>
2024-09-16 10:33:23 +0200

    Improve search

--- Quack/Macros/MacroSearch.cs
+++ Quack/Macros/MacroSearch.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 using Dalamud.Utility;
+using System.ComponentModel.DataAnnotations;
 
 namespace Quack.Macros;
 public class MacroSearch
@@ -9,19 +10,23 @@ public class MacroSearch
     private static readonly StringComparison MATCH_FLAGS = StringComparison.InvariantCultureIgnoreCase;
     public static List<Macro> Lookup(IEnumerable<Macro> macros, string filter)
     {
-        return new(macros
-            .Where(m => CountTokenMatches(m, filter) > 0)
-            .OrderBy(m => -CountTokenMatches(m, filter))
-            .ThenBy(x => x.Name));
+        return new(macros.Where(m => tokenMatches(m, filter)).OrderBy(m => matchLengthRatio(m, filter)));
     }
 
-    private static int CountTokenMatches(Macro macro, string filter)
+    private static bool tokenMatches(Macro macro, string filter)
     {
-        var tokens = filter.ToLowerInvariant().Split(" ");
-        return tokens.Count(t => macro.Name.Contains(t, MATCH_FLAGS) ||
+        var tokens = filter.ToLowerInvariant().Split(" ").Where(t => !t.IsNullOrWhitespace());
+        return tokens.All(t => macro.Name.Contains(t, MATCH_FLAGS) ||
                                macro.Path.Contains(t, MATCH_FLAGS) ||
                                macro.Command.Contains(t, MATCH_FLAGS) ||
                                macro.Tags.Any(tag => tag.Contains(t, MATCH_FLAGS)));
+    }
+
+    private static double matchLengthRatio(Macro macro, string filter)
+    {
+        var length = macro.Name.Length + macro.Path.Length + macro.Command.Length + string.Join("", macro.Tags).Length;
+        var ratio = (double)length / filter.Length;
+        return Math.Round(ratio, 0);
     }
 
     public static Macro? FindByNameOrPath(IEnumerable<Macro> macros, string nameOrPath)
