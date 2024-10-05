08f625e1509c87ca9cb2d6e43039b20fd4f04ee5
anya-hichu <anya_hichu@outlook.com>
2024-10-05 21:06:41 +0200

    smol improv

--- Quack/Generators/GeneratorConfig.cs
+++ Quack/Generators/GeneratorConfig.cs
@@ -191,14 +191,15 @@ function normalize(path) {
 function main(emotesJson) {
     const emotes = JSON.parse(emotesJson);
     const macros = emotes.flatMap(e => {
+        var categoryName = `${e.category[0].toUpperCase()}${e.category.slice(1)}`;
         return [{
             name: `Emote [${e.name}]`,
-            path: `Emotes/${e.category[0].toUpperCase()}${e.category.slice(1)}/${e.name}/Execute`,
+            path: `Emotes/${categoryName}/${e.name}/Execute`,
             tags: ['emote', e.category.toLowerCase(), e.command],
             content: e.command
         }, {
             name: `Emote [${e.name}] [Motion]`,
-            path: `Emotes/${e.category[0].toUpperCase()}${e.category.slice(1)}/${e.name}/Execute [motion]`,
+            path: `Emotes/${categoryName}/${e.name}/Execute [motion]`,
             tags: ['emote', e.category.toLowerCase(), e.command],
             content: `${e.command} motion`
         }];
